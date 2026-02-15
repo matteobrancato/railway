a99621d88c3f037fd538c7fdeebbc56d2419193c
Matteo Brancato <matteobrancato@icloud.com>
2026-02-15 23:34:43 +0100

    Slight efficency enhancements

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-import streamlit as st
-import requests
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
+import requests
+import streamlit as st
 
 # ─────────────────────────────────────────────────────────────
 # CONFIGURATION
